b2b98e4f6101d53aff704c6b95230e872e51168f
Kamui2134 <ctorkey@gmail.com>
2024-04-10 14:30:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import {} from "@/scripts/WorkWithCookies.js"
+
+export default {
+	data() {},
+	mounted() {
+		this.$nextTick(function () {
+			// специальные символы (пробелы), требуется кодирование
+			let name = 'my name'
+			let value = 'John Smith'
+
+			// кодирует в my%20name=John%20Smith
+			document.cookie =
+				encodeURIComponent(name) + '=' + encodeURIComponent(value)
+		})
+	},
+	methods: {
+		
+	},
+}
+</script>
+
+<template>
+	<div>привет</div>
+</template>
+
+<style scoped></style>
+@/scripts/WorkWithCookies
